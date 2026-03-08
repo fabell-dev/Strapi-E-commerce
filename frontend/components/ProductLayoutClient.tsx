3c5ca5846d3c0029292ef60b6d3d87a996c60cbe
fabell-dev <ojeafabian66@gmail.com>
2026-03-07 19:11:52 -0500

    product placeholder enought for today

--- frontend/components/ProductLayoutClient.tsx
+++ frontend/components/ProductLayoutClient.tsx
@@ -7,5 +7,15 @@ type Props = {
 const STRAPI_HOST = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 export default function ProductLayoutClient({ product }: Props) {
-  return <img src={`${STRAPI_HOST}${product.image.url}`}></img>;
+  return (
+    <>
+      <div className="flex mt-50 items-center justify-center gap-10">
+        <img
+          className="w-80 h-80"
+          src={`${STRAPI_HOST}${product.image.url}`}
+        ></img>
+        <p className="font-bold">{product.name}</p>
+      </div>
+    </>
+  );
 }
