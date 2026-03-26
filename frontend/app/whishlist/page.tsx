4604c096e3174a6573e68280c242ee37c860192e
fabell-dev <ojeafabian66@gmail.com>
2026-03-25 22:13:34 -0400

    please?

--- frontend/app/whishlist/page.tsx
+++ frontend/app/whishlist/page.tsx
@@ -11,7 +11,7 @@ export default async function page({
 }) {
   const paramsPagination = await searchParams;
   if (!paramsPagination.page || !paramsPagination.pageSize) {
-    redirect("?page=1&pageSize=9");
+    redirect("?page=1&pageSize=8");
   }
   const paramData = await searchParams;
   const page = parseInt(
